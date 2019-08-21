31db7e031168f533f98893c2553e68c31f25b1ad
JIHOON LEE <eziya76@gmail.com>
2019-08-21 20:51:54 +0900

    update
    update

--- PIC18F_CCP1_PWM.X/main.c
+++ PIC18F_CCP1_PWM.X/main.c
@@ -1,42 +1,63 @@
 #include "main.h"
 
-void main(void) {
+volatile uint8_t flagINT0 = 0;
 
+void __interrupt(high_priority) highISR(void) {
+    if (INTCONbits.INT0IF) {
+        
+        __delay_ms(100);    //button debounce
+        
+        if (INTCONbits.INT0IF) {
+            flagINT0 = 1; //switch direction            
+        }
+
+        INTCONbits.INT0IF = 0;
+    }
+}
+
+void main(void) {
     uint8_t duty;
     uint16_t dutyVal;
 
     OSCCON = 0x72; //16MHz, Internal OSC
 
-    ANSEL = 0x0; //disable analog input     
+    ANSEL = 0x0; //disable analog input    
     ANSELH = 0x0;
 
+    INTCONbits.INT0IF = 0; //clear INT0 flag
+    INTCON2bits.INTEDG0 = 1; //rising edge
+    INTCONbits.INT0IE = 1; //enable INT0
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
+
     TRISCbits.RC2 = 0; //CCP1 P1A PWM output
-    TRISDbits.RD5 = 0; //CCP1 P1B PWM output 
+    TRISDbits.RD5 = 0; //CCP1 P1B PWM output
     TRISDbits.RD6 = 0; //CCP1 P1C PWM output
     TRISDbits.RD7 = 0; //CCP1 P1D PWM output
 
     CCP1CONbits.CCP1M = 0xC; //PWM mode, active high    
-    CCP1CONbits.P1M = 0; //single output (steering mode)
-    
-    PSTRCONbits.STRA = 1; //P1A pin outout
-    PSTRCONbits.STRB = 1; //P1B pin outout
-    PSTRCONbits.STRC = 1; //P1B pin outout
-    PSTRCONbits.STRD = 1; //P1B pin outout
+    CCP1CONbits.P1M = 1; //full bridge forward
 
     T2CONbits.T2CKPS = 2; //Prescaler 1:16, 16MHz / 4 / 16 = 250kHz
-    PR2 = 249; //4us * 250 = 1ms, PWM Freq = 1kHz       
-
+    PR2 = 249; //4us * 250 = 1ms, PWM Freq = 1kHz      
     duty = 70; //duty ration 70%
     dutyVal = (PR2 + 1) * 4 / 100 * duty; //4*(PR2+1)*Duty Ratio
-
     CCPR1L = dutyVal >> 2; //duty registers
     CCP1CONbits.DC1B = (dutyVal & 0x03); //duty registers
 
     T2CONbits.TMR2ON = 1;
-
     while (1) {
+        if (flagINT0) {
+            flagINT0 = 0;
 
+            CCP1CONbits.CCP1M = 0x0; //stop pwm
+            __delay_ms(5);
+
+            if (CCP1CONbits.P1M == 1) CCP1CONbits.P1M = 3; //switch to reverse
+            else CCP1CONbits.P1M = 1; //switch to forward
+
+            CCP1CONbits.CCP1M = 0xC; //enable pwm
+        }
     }
-    
     return;
 }
