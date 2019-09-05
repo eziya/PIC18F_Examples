b6d8c4c9b3ab3ad6fa4b243d40e9ebd12fabc1a7
JIHOON LEE <eziya76@gmail.com>
2019-09-06 00:08:21 +0900

    update
    update

--- PIC18F_SPI_AT45DBXX.X/nbproject/Makefile-default.mk
+++ PIC18F_SPI_AT45DBXX.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c spi_master.c uart.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c spi_master.c uart.c at45dbxx.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/spi_master.p1 ${OBJECTDIR}/uart.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/spi_master.p1.d ${OBJECTDIR}/uart.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/spi_master.p1 ${OBJECTDIR}/uart.p1 ${OBJECTDIR}/at45dbxx.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/spi_master.p1.d ${OBJECTDIR}/uart.p1.d ${OBJECTDIR}/at45dbxx.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/spi_master.p1 ${OBJECTDIR}/uart.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/spi_master.p1 ${OBJECTDIR}/uart.p1 ${OBJECTDIR}/at45dbxx.p1
 
 # Source Files
-SOURCEFILES=main.c spi_master.c uart.c
+SOURCEFILES=main.c spi_master.c uart.c at45dbxx.c
 
 
 CFLAGS=
@@ -117,6 +117,14 @@ ${OBJECTDIR}/uart.p1: uart.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/uart.d ${OBJECTDIR}/uart.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/uart.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/at45dbxx.p1: at45dbxx.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/at45dbxx.p1.d 
+	@${RM} ${OBJECTDIR}/at45dbxx.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -I"D:/Program Files (x86)/Microchip/xc8/v2.05/pic/include/c99" -I"D:/Program Files (x86)/Microchip/xc8/v2.05/pic/include" -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/at45dbxx.p1 at45dbxx.c 
+	@-${MV} ${OBJECTDIR}/at45dbxx.d ${OBJECTDIR}/at45dbxx.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/at45dbxx.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -141,6 +149,14 @@ ${OBJECTDIR}/uart.p1: uart.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -I"D:/Program Files (x86)/Microchip/xc8/v2.05/pic/include/c99" -I"D:/Program Files (x86)/Microchip/xc8/v2.05/pic/include" -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/uart.p1 uart.c 
 	@-${MV} ${OBJECTDIR}/uart.d ${OBJECTDIR}/uart.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/uart.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/at45dbxx.p1: at45dbxx.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/at45dbxx.p1.d 
+	@${RM} ${OBJECTDIR}/at45dbxx.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -I"D:/Program Files (x86)/Microchip/xc8/v2.05/pic/include/c99" -I"D:/Program Files (x86)/Microchip/xc8/v2.05/pic/include" -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/at45dbxx.p1 at45dbxx.c 
+	@-${MV} ${OBJECTDIR}/at45dbxx.d ${OBJECTDIR}/at45dbxx.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/at45dbxx.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
